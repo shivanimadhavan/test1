2fed05fe26e37cbc9823de7d18c290e0839a2529
Shivani.M <66153367+shivanimadhavan@users.noreply.github.com>
2021-05-10 21:58:15 +0530

    Add files via upload

---
+++ Assessment3/Number4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assesment3
+{
+    class Question4
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the numbers");
+            var numbers = new List<int>(Convert.ToInt32(Console.ReadLine()));
+            int input = Convert.ToInt32(Console.ReadLine());
+            while (input != 0)
+            {
+                if (input > 0)
+                    numbers.Add(input);
+                input = Convert.ToInt32(Console.ReadLine());
+
+            }
+            numbers.Sort();
+            foreach (var item in numbers)
+
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Assessment3/Number5.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Assessment
+//{
+//    class Number5
+//    {
+//        public static void Main()
+//        {
+//            string username, password;
+//            int ctr = 0;
+//            Console.Write("\n\nCheck username and password :\n");
+
+//            do
+//            {
+//                Console.Write("Input a username: ");
+//                username = Console.ReadLine();
+
+//                Console.Write("Input a password: ");
+//                password = Console.ReadLine();
+
+//                if (username != "Admin" || password != "admin")
+//                {
+//                    Console.WriteLine("Incorrect username or password.Try again");
+//                    ctr++;
+//                }
+//                else
+//                    Console.WriteLine("Welcome");
+//                ctr = 1;
+
+
+//            }
+//            while ((username != "Admin" || password != "admin") && (ctr != 3));
+
+//            if (ctr == 3)
+//                Console.Write("\nLogin attempt three or more times. Try later!\n\n");
+//            else
+//                Console.Write("\nThe password entered successfully!\n\n");
+//        }
+//    }
+//}
